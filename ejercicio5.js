26afccb0b3379da747429b70670dee999a70462c
gutijaume <gutijaume@hotmail.com>
2021-12-05 19:25:58 +0100

    Create ejercicio5.js
    Muestro aquellos equipos que tienen más de 25 puntos

---
+++ ejercicio5.js
@@ -0,0 +1,94 @@
+//5.- Mostrar los equipos con más de 25 puntos
+
+const ligaSantander = [
+    {
+      class: 1,
+      nombre: "Real Madrid",
+      winner: true,
+      mannager: "Carlo Ancelotti",
+      bestPlayers: ["Vinicius", "Benzema"],
+      points: 30,
+    },
+    {
+      class: 8,
+      nombre: "Athletic",
+      winner: true,
+      mannager: "Marcelino",
+      bestPlayers: ["Muniain", "Iñaki Williams", "Iñigo Martinez"],
+      points: 20,
+    },
+    {
+      class: 4,
+      nombre: "Atletico de Madrid",
+      winner: true,
+      mannager: "Cholo Simeone",
+      bestPlayers: ["Carrasco", "Luis Suarez", "Correa"],
+      points: 27,
+    },
+    {
+      class: 5,
+      nombre: "Betis",
+      winner: true,
+      mannager: "Manuel Pellegrini",
+      bestPlayers: ["Fekir", "Juanmi", "Canales"],
+      points: 27,
+    },
+    {
+      class: 3,
+      nombre: "Sevilla",
+      winner: true,
+      mannager: "Julen Lopetegi",
+      bestPlayers: ["Acuña"],
+      points: 28,
+    },
+    {
+      class: 6,
+      nombre: "Rayo Vallecano",
+      winner: false,
+      mannager: "Andoni Iraola",
+      bestPlayers: [
+        "Alvaro Garcia",
+        "Trejo",
+        "Isi Palazon",
+        "Comesaña",
+        "Fran Garcia",
+        "Dimitrievski",
+      ],
+      points: 24,
+    },
+    {
+      class: 2,
+      nombre: "Real Sociedad",
+      winner: true,
+      mannager: "Imanol Alguacil",
+      bestPlayers: ["Mikel Merino", "Aritz", "Oyarzabal", "Le Normand"],
+      points: 29,
+    },
+    {
+      class: 9,
+      nombre: "Espanyol",
+      winner: false,
+      mannager: "Vicente Moreno",
+      bestPlayers: ["Raul de Tomas", "Embarba", "Diego Lopez", "Pedrosa"],
+      points: 20,
+    },
+    {
+      class: 7,
+      nombre: "FC Barcelona",
+      winner: true,
+      mannager: "Xavi Hernandez",
+      bestPlayers: ["Depay", "Busquets"],
+      points: 23,
+    },
+  ];
+
+
+  //1. Opción FILTER
+
+  let showTheBests = (data) =>{
+      let theBests = data.filter((team)=>team.points > 25) //Si tuviese que ejectuar más de una línea necesitaria llaves y un return
+      console.log(theBests)
+  }
+
+  showTheBests(ligaSantander)
+
