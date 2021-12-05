2be309780cb360df87650e07fabeab447da81850
gutijaume <gutijaume@hotmail.com>
2021-12-05 13:13:20 +0100

    Create ejercicio6.js
    Vamos a ver si Vinicius es determinante.  Para ello buscaremos en que puesto de la clasificación está el equipo que cuenta con el jugador brasileño entre sus "Mejores Jugadores

---
+++ ejercicio6.js
@@ -0,0 +1,97 @@
+//6.- Mostrar la clasificación del equipo que cuenta con "Vinicius" entre sus mejores jugadors//
+
+//Si por ejemplo me dijese que quiere que muestre la clasificación de los equipos que han ganado la liga debería usar el FILTER
+// Con el FIND solo consigue el primer valor que encuentra en el array
+
+const ligaSantander = [
+    {
+      class: 1,
+      nombre: "Real Madrid",
+      winner: true,
+      mannager: "Carlo Ancelotti",
+      bestPlayers: ["Vinicius", "Benzema"],
+      points: 30,
+    },
+    {
+      class: 8,
+      nombre: "Athletic",
+      winner: true,
+      mannager: "Marcelino",
+      bestPlayers: ["Muniain", "Iñaki Williams", "Iñigo Martinez"],
+      points: 20,
+    },
+    {
+      class: 4,
+      nombre: "Atletico de Madrid",
+      winner: true,
+      mannager: "Cholo Simeone",
+      bestPlayers: ["Carrasco", "Luis Suarez", "Correa"],
+      points: 27,
+    },
+    {
+      class: 5,
+      nombre: "Betis",
+      winner: true,
+      mannager: "Manuel Pellegrini",
+      bestPlayers: ["Fekir", "Juanmi", "Canales"],
+      points: 27,
+    },
+    {
+      class: 3,
+      nombre: "Sevilla",
+      winner: true,
+      mannager: "Julen Lopetegi",
+      bestPlayers: ["Acuña"],
+      points: 28,
+    },
+    {
+      class: 6,
+      nombre: "Rayo Vallecano",
+      winner: false,
+      mannager: "Andoni Iraola",
+      bestPlayers: [
+        "Alvaro Garcia",
+        "Trejo",
+        "Isi Palazon",
+        "Comesaña",
+        "Fran Garcia",
+        "Dimitrievski",
+      ],
+      points: 24,
+    },
+    {
+      class: 2,
+      nombre: "Real Sociedad",
+      winner: true,
+      mannager: "Imanol Alguacil",
+      bestPlayers: ["Mikel Merino", "Aritz", "Oyarzabal", "Le Normand"],
+      points: 29,
+    },
+    {
+      class: 9,
+      nombre: "Espanyol",
+      winner: false,
+      mannager: "Vicente Moreno",
+      bestPlayers: ["Raul de Tomas", "Embarba", "Diego Lopez", "Pedrosa"],
+      points: 20,
+    },
+    {
+      class: 7,
+      nombre: "FC Barcelona",
+      winner: true,
+      mannager: "Xavi Hernandez",
+      bestPlayers: ["Depay", "Busquets"],
+      points: 23,
+    },
+  ];
+
+    let viniciusTeamClassification = (data) => {
+      let foundPlayer = data.find((team)=> team.bestPlayers.includes("Vinicius"))
+      if (foundPlayer){
+          console.log(foundPlayer.class)
+      } else {
+          console.log("Vinicius no está en ningún equipo")
+      }
+  }
+
+  viniciusTeamClassification(ligaSantander)
